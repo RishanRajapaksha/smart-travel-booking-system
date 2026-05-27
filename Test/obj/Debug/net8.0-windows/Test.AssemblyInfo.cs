53c57f214f0835dadf1f158d3bbbd94cb8c0e6eb
Vikasitha444 <pubuth@gmail.com>
2026-05-27 17:51:43 +0530

    Added “Error Handling” Part to “GoogleUserLogin()”.

--- Test/obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3856723877ac8bff4bca87dbeab8a08440d1b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd59895812190e8d6969bd2b247ffc2ef20956f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
